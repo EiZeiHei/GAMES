bd84a1fe93359db69bb739eeeffa295136afc003
EiZeiHei <lengao7@outlook.com>
2022-03-28 20:54:47 +0800

    Unity

---
+++ GAMES103/GAMES103/Assets/lab1_bunny/Rigid_Bunny.cs
@@ -0,0 +1,204 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rigid_Bunny : MonoBehaviour 
+{
+	bool launched = false;
+	
+	float dt = 0.015f;
+
+	Vector3 v = new Vector3(0, 0, 0);	// velocity
+	Vector3 w = new Vector3(0, 0, 0);	// angular velocity
+	
+	float mass;							// mass
+	Matrix4x4 I_ref;					// reference inertia
+
+	float linear_decay	= 0.999f;		// for damping
+	float angular_decay	= 0.98f;
+	float muN 		= 0.5f;     // for collision
+	float muT = 0.1f;
+	Vector3 g = new Vector3(0, -9.81f, 0);	//gravity
+
+	// Use this for initialization
+	void Start () 
+	{		
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		Vector3[] vertices = mesh.vertices;
+
+		float m=1;
+		mass=0;
+		for (int i=0; i<vertices.Length; i++) 
+		{
+			mass += m;
+			float diag=m*vertices[i].sqrMagnitude;
+			I_ref[0, 0]+=diag;
+			I_ref[1, 1]+=diag;
+			I_ref[2, 2]+=diag;
+			I_ref[0, 0]-=m*vertices[i][0]*vertices[i][0];
+			I_ref[0, 1]-=m*vertices[i][0]*vertices[i][1];
+			I_ref[0, 2]-=m*vertices[i][0]*vertices[i][2];
+			I_ref[1, 0]-=m*vertices[i][1]*vertices[i][0];
+			I_ref[1, 1]-=m*vertices[i][1]*vertices[i][1];
+			I_ref[1, 2]-=m*vertices[i][1]*vertices[i][2];
+			I_ref[2, 0]-=m*vertices[i][2]*vertices[i][0];
+			I_ref[2, 1]-=m*vertices[i][2]*vertices[i][1];
+			I_ref[2, 2]-=m*vertices[i][2]*vertices[i][2];
+		}
+		I_ref [3, 3] = 1;
+	}
+	
+	Matrix4x4 Get_Cross_Matrix(Vector3 a)
+	{
+		//Get the cross product matrix of vector a
+		Matrix4x4 A = Matrix4x4.zero;
+		A [0, 0] = 0; 
+		A [0, 1] = -a [2]; 
+		A [0, 2] = a [1]; 
+		A [1, 0] = a [2]; 
+		A [1, 1] = 0; 
+		A [1, 2] = -a [0]; 
+		A [2, 0] = -a [1]; 
+		A [2, 1] = a [0]; 
+		A [2, 2] = 0; 
+		A [3, 3] = 1;
+		return A;
+	}
+
+	Matrix4x4 Add_Matrix(Matrix4x4 A, Matrix4x4 B)
+	{
+		// Get the sum of matrices A and B
+		Matrix4x4 C = Matrix4x4.zero;
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				C[i, j] = A[i, j] + B[i, j];
+			}
+		}
+		return C;
+	}
+
+	Matrix4x4 Prod_Matrix(Matrix4x4 A, float a)
+	{
+		// Get the component-wise product of a matrix with a scalar
+		Matrix4x4 B = Matrix4x4.zero;
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				B[i, j] = A[i, j] * a;
+			}
+		}
+		return B;
+	}
+
+	void Collision_Handler(Vector3 P, Vector3 N, float dt)
+	{
+        Matrix4x4 R = Matrix4x4.TRS(new Vector3(0, 0, 0), transform.rotation, new Vector3(1, 1, 1));
+        Vector3 x = transform.position;
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+
+        Vector3 va = new Vector3(0.0f, 0.0f, 0.0f);
+        Vector3 Ra = new Vector3(0.0f, 0.0f, 0.0f);
+        int num = 0;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            //Collision detection
+            Vector3 Rr = R * vertices[i];
+            Vector3 xi = x + Rr;
+            float Phi = Vector3.Dot((xi - P), N);
+
+            if (Phi < 0)
+            {
+				Vector3 tmp = Vector3.Cross(w, Rr);
+                Vector3 vi = v + tmp;
+                if (Vector3.Dot(vi, N) < 0)
+                {
+                    va += vi;
+                    Ra += Rr;
+                    num++;
+                }
+            }
+        }
+        if (num > 0)
+        {
+            va /= num;
+            Ra /= num;
+            if (va.magnitude < 4.0f * dt)
+            {
+				muN = 0;
+            }
+
+			Matrix4x4 I_now = R * I_ref * R.transpose;
+
+			//Compute the wanted van
+			Vector3 vN = Vector3.Dot(va, N) * N;
+            Vector3 vT = va - vN;
+            float a = Mathf.Max(1 - (muT * (1 + muN) * vN.magnitude / vT.magnitude), 0);
+
+            vN = - muN * vN;
+            vT = a * vT;
+            Vector3 van = vN + vT;
+
+            //Compute the impulse j
+            Matrix4x4 I = Matrix4x4.identity;
+            Matrix4x4 Rrx = Get_Cross_Matrix(Ra);
+
+            Matrix4x4 K = Add_Matrix(Prod_Matrix(I, 1.0f / mass), Prod_Matrix(Rrx * I_now.inverse * Rrx, -1));
+            Vector3 j = K.inverse * (van - va);
+
+            //Update v and w
+            v = v + 1 / mass * j;
+            Vector3 tmp = I_now.inverse * Vector3.Cross(Ra, j);
+            w = w + tmp;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update () 
+	{
+		if(Input.GetKey("r"))
+		{
+			transform.position = new Vector3 (0, 0.6f, 0);
+			muN = 0.5f;
+			launched=false;
+		}
+		if(Input.GetKey("l"))
+		{
+			v = new Vector3 (3, 2, 0);
+			launched=true;
+		}
+
+        if (launched)
+        {
+			// Part I: Update velocities
+			v += dt * g;
+			v *= linear_decay;
+			w *= angular_decay;
+
+			// Part II: Collision Handler
+			Collision_Handler(new Vector3(0, 0.01f, 0), new Vector3(0, 1, 0), dt);
+			Collision_Handler(new Vector3(2, 0, 0), new Vector3(-1, 0, 0), dt);
+
+			// Part III: Update position & orientation
+			//Update linear status
+			Vector3 x = transform.position;
+			x += dt * v;
+			//Update angular status
+			Quaternion q = transform.rotation;
+			Quaternion tmp = new Quaternion(w.x, w.y, w.z, 0);
+			tmp = tmp * q;
+			q.x += 0.5f * dt * tmp.x;
+			q.y += 0.5f * dt * tmp.y;
+			q.z += 0.5f * dt * tmp.z;
+			q.w += 0.5f * dt * tmp.w;
+
+			// Part IV: Assign to the bunny object
+			transform.position = x;
+			transform.rotation = q;
+		}
+	}
+}
---
+++ GAMES103/GAMES103/Assets/lab3_house/FVM.cs
@@ -0,0 +1,339 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class FVM : MonoBehaviour
+{
+    public bool SVD = false;
+
+	float dt 			= 0.003f;
+    float mass 			= 1;
+	float stiffness_0	= 20000.0f;
+    float stiffness_1 	= 5000.0f;
+    float damp			= 0.999f;
+
+	int[] 		Tet;
+	int tet_number;			//The number of tetrahedra
+
+	Vector3[] 	Force;
+	Vector3[] 	V;
+	Vector3[] 	X;
+	int number;				//The number of vertices
+
+	Matrix4x4[] inv_Dm;
+
+	//For Laplacian smoothing.
+	Vector3[]   V_sum;
+	int[]		V_num;
+
+	SVD svd = new SVD();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	// FILO IO: Read the house model from files.
+    	// The model is from Jonathan Schewchuk's Stellar lib.
+    	{
+    		string fileContent = File.ReadAllText("Assets/lab3_house/house2.ele");
+    		string[] Strings = fileContent.Split(new char[]{' ', '\t', '\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
+    		
+    		tet_number=int.Parse(Strings[0]);
+        	Tet = new int[tet_number*4];
+
+    		for(int tet=0; tet<tet_number; tet++)
+    		{
+				Tet[tet*4+0]=int.Parse(Strings[tet*5+4])-1;
+				Tet[tet*4+1]=int.Parse(Strings[tet*5+5])-1;
+				Tet[tet*4+2]=int.Parse(Strings[tet*5+6])-1;
+				Tet[tet*4+3]=int.Parse(Strings[tet*5+7])-1;
+			}
+    	}
+    	{
+			string fileContent = File.ReadAllText("Assets/lab3_house/house2.node");
+    		string[] Strings = fileContent.Split(new char[]{' ', '\t', '\r', '\n'}, StringSplitOptions.RemoveEmptyEntries);
+    		number = int.Parse(Strings[0]);
+    		X = new Vector3[number];
+       		for(int i=0; i<number; i++)
+       		{
+       			X[i].x=float.Parse(Strings[i*5+5])*0.4f;
+       			X[i].y=float.Parse(Strings[i*5+6])*0.4f;
+       			X[i].z=float.Parse(Strings[i*5+7])*0.4f;
+       		}
+    		//Centralize the model.
+	    	Vector3 center=Vector3.zero;
+	    	for(int i=0; i<number; i++)		center+=X[i];
+	    	center=center/number;
+	    	for(int i=0; i<number; i++)
+	    	{
+	    		X[i]-=center;
+	    		float temp=X[i].y;
+	    		X[i].y=X[i].z;
+	    		X[i].z=temp;
+	    	}
+		}
+        /*tet_number=1;
+        Tet = new int[tet_number*4];
+        Tet[0]=0;
+        Tet[1]=1;
+        Tet[2]=2;
+        Tet[3]=3;
+
+        number=4;
+        X = new Vector3[number];
+        V = new Vector3[number];
+        Force = new Vector3[number];
+        X[0]= new Vector3(0, 0, 0);
+        X[1]= new Vector3(1, 0, 0);
+        X[2]= new Vector3(0, 1, 0);
+        X[3]= new Vector3(0, 0, 1);*/
+
+
+        //Create triangle mesh.
+       	Vector3[] vertices = new Vector3[tet_number*12];
+        int vertex_number=0;
+        for(int tet=0; tet<tet_number; tet++)
+        {
+        	vertices[vertex_number++]=X[Tet[tet*4+0]];
+        	vertices[vertex_number++]=X[Tet[tet*4+2]];
+        	vertices[vertex_number++]=X[Tet[tet*4+1]];
+
+        	vertices[vertex_number++]=X[Tet[tet*4+0]];
+        	vertices[vertex_number++]=X[Tet[tet*4+3]];
+        	vertices[vertex_number++]=X[Tet[tet*4+2]];
+
+        	vertices[vertex_number++]=X[Tet[tet*4+0]];
+        	vertices[vertex_number++]=X[Tet[tet*4+1]];
+        	vertices[vertex_number++]=X[Tet[tet*4+3]];
+
+        	vertices[vertex_number++]=X[Tet[tet*4+1]];
+        	vertices[vertex_number++]=X[Tet[tet*4+2]];
+        	vertices[vertex_number++]=X[Tet[tet*4+3]];
+        }
+
+        int[] triangles = new int[tet_number*12];
+        for(int t=0; t<tet_number*4; t++)
+        {
+        	triangles[t*3+0]=t*3+0;
+        	triangles[t*3+1]=t*3+1;
+        	triangles[t*3+2]=t*3+2;
+        }
+        Mesh mesh = GetComponent<MeshFilter> ().mesh;
+		mesh.vertices  = vertices;
+		mesh.triangles = triangles;
+		mesh.RecalculateNormals ();
+
+
+		V 	  = new Vector3[number];
+        Force = new Vector3[number];
+        V_sum = new Vector3[number];
+        V_num = new int[number];
+
+        //TODO: Need to allocate and assign inv_Dm
+        inv_Dm = new Matrix4x4[tet_number];
+        for (int i = 0; i < tet_number; i++)
+        {
+            inv_Dm[i] = Build_Edge_Matrix(i).inverse;
+        }
+    }
+
+    Matrix4x4 Build_Edge_Matrix(int tet)
+    {
+    	Matrix4x4 ret=Matrix4x4.zero;
+        //TODO: Need to build edge matrix here.
+        ret[0, 0] = X[Tet[tet * 4 + 1]].x - X[Tet[tet * 4 + 0]].x;
+        ret[1, 0] = X[Tet[tet * 4 + 1]].y - X[Tet[tet * 4 + 0]].y;
+        ret[2, 0] = X[Tet[tet * 4 + 1]].z - X[Tet[tet * 4 + 0]].z;
+        ret[3, 0] = 0;
+
+        ret[0, 1] = X[Tet[tet * 4 + 2]].x - X[Tet[tet * 4 + 0]].x;
+        ret[1, 1] = X[Tet[tet * 4 + 2]].y - X[Tet[tet * 4 + 0]].y;
+        ret[2, 1] = X[Tet[tet * 4 + 2]].z - X[Tet[tet * 4 + 0]].z;
+        ret[3, 1] = 0;
+
+        ret[0, 2] = X[Tet[tet * 4 + 3]].x - X[Tet[tet * 4 + 0]].x;
+        ret[1, 2] = X[Tet[tet * 4 + 3]].y - X[Tet[tet * 4 + 0]].y;
+        ret[2, 2] = X[Tet[tet * 4 + 3]].z - X[Tet[tet * 4 + 0]].z;
+        ret[3, 2] = 0;
+
+        ret[0, 3] = 0;
+        ret[1, 3] = 0;
+        ret[2, 3] = 0;
+        ret[3, 3] = 1;
+
+        return ret;
+    }
+
+    void Smooth()
+    {
+        for (int i = 0; i < number; i++)
+        {
+            V_sum[i] = new Vector3(0, 0, 0);
+            V_num[i] = 0;
+        }
+
+        for (int tet = 0; tet < tet_number; tet++)
+        {
+            Vector3 sum = V[Tet[tet * 4 + 0]] + V[Tet[tet * 4 + 1]] + V[Tet[tet * 4 + 2]] + V[Tet[tet * 4 + 3]];
+            V_sum[Tet[tet * 4 + 0]] += sum;
+            V_sum[Tet[tet * 4 + 1]] += sum;
+            V_sum[Tet[tet * 4 + 2]] += sum;
+            V_sum[Tet[tet * 4 + 3]] += sum;
+            V_num[Tet[tet * 4 + 0]] += 4;
+            V_num[Tet[tet * 4 + 1]] += 4;
+            V_num[Tet[tet * 4 + 2]] += 4;
+            V_num[Tet[tet * 4 + 3]] += 4;
+        }
+
+        for (int i = 0; i < number; i++)
+        {
+            V[i] = 0.9f * V[i] + 0.1f * V_sum[i] / V_num[i];
+        }
+    }
+
+    void _Update()
+    {
+    	// Jump up.
+		if(Input.GetKeyDown(KeyCode.Space))
+    	{
+    		for(int i=0; i<number; i++)
+    			V[i].y+=0.2f;
+    	}
+
+    	for(int i=0 ;i<number; i++)
+    	{
+    		//TODO: Add gravity to Force.
+            Force[i] = new Vector3(0, -9.81f * mass, 0);
+    	}
+
+    	for(int tet=0; tet<tet_number; tet++)
+    	{
+    		//TODO: Deformation Gradient
+    		Matrix4x4 edge = Build_Edge_Matrix (tet);
+            Matrix4x4 F = edge * inv_Dm[tet];
+            Matrix4x4 tmp = Matrix4x4.zero;
+
+            if (!SVD)
+            {
+                //TODO: Green Strain
+                Matrix4x4 G = F.transpose * F;
+                G[0, 0] -= 1;
+                G[1, 1] -= 1;
+                G[2, 2] -= 1;
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        G[i, j] *= 0.5f;
+                    }
+                }
+
+                //TODO: Second PK Stress
+                Matrix4x4 S = Matrix4x4.zero;
+                float trace = G[0, 0] + G[1, 1] + G[2, 2];
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        S[i, j] = 2 * stiffness_1 * G[i, j];
+                    }
+                }
+                S[0, 0] += stiffness_0 * trace;
+                S[1, 1] += stiffness_0 * trace;
+                S[2, 2] += stiffness_0 * trace;
+
+                //Matrix4x4 S = Add_Matrix(Prod_Matrix(G, 2.0f * stiffness_0), Prod_Matrix(Matrix4x4.identity, stiffness_1 * trace));
+                Matrix4x4 P = F * S;
+
+                //TODO: Elastic Force
+                tmp = P * inv_Dm[tet].transpose;
+            }
+            else
+            {
+                Matrix4x4 U = Matrix4x4.identity;
+                Matrix4x4 D = Matrix4x4.identity;
+                Matrix4x4 V = Matrix4x4.identity;
+                svd.svd(F, ref U, ref D, ref V);
+
+                Matrix4x4 P = Matrix4x4.identity;
+                float I = D[0, 0] * D[0, 0] + D[1, 1] * D[1, 1] + D[2, 2] * D[2, 2];
+                float J = D[0, 0] * D[1, 1] * D[2, 2];
+                float II = D[0, 0] * D[0, 0] * D[0, 0] * D[0, 0] + D[1, 1] * D[1, 1] * D[1, 1] * D[1, 1] + D[2, 2] * D[2, 2] * D[2, 2] * D[2, 2];
+                float III = J * J;
+                float dEdI = stiffness_0 * (I - 3) * 0.25f - stiffness_1 * 0.5f;
+                float dEdII = stiffness_1 * 0.25f;
+                float dEdIII = 0;
+
+                P[0, 0] = 2 * dEdI * D[0, 0] + 4 * dEdII * D[0, 0] * D[0, 0] * D[0, 0] + 2 * dEdIII * III / D[0, 0];
+                P[1, 1] = 2 * dEdI * D[1, 1] + 4 * dEdII * D[1, 1] * D[1, 1] * D[1, 1] + 2 * dEdIII * III / D[1, 1];
+                P[2, 2] = 2 * dEdI * D[2, 2] + 4 * dEdII * D[2, 2] * D[2, 2] * D[2, 2] + 2 * dEdIII * III / D[2, 2];
+
+                tmp = U * P * V.transpose * inv_Dm[tet].transpose;
+            }
+
+            float k = -1 / (inv_Dm[tet].determinant * 6);
+
+            Force[Tet[tet * 4 + 0]].x -= k * (tmp[0, 0] + tmp[0, 1] + tmp[0, 2]);
+            Force[Tet[tet * 4 + 0]].y -= k * (tmp[1, 0] + tmp[1, 1] + tmp[1, 2]);
+            Force[Tet[tet * 4 + 0]].z -= k * (tmp[2, 0] + tmp[2, 1] + tmp[2, 2]);
+            Force[Tet[tet * 4 + 1]].x += k * tmp[0, 0];
+            Force[Tet[tet * 4 + 1]].y += k * tmp[1, 0];
+            Force[Tet[tet * 4 + 1]].z += k * tmp[2, 0];
+            Force[Tet[tet * 4 + 2]].x += k * tmp[0, 1];
+            Force[Tet[tet * 4 + 2]].y += k * tmp[1, 1];
+            Force[Tet[tet * 4 + 2]].z += k * tmp[2, 1];
+            Force[Tet[tet * 4 + 3]].x += k * tmp[0, 2];
+            Force[Tet[tet * 4 + 3]].y += k * tmp[1, 2];
+            Force[Tet[tet * 4 + 3]].z += k * tmp[2, 2];
+        }
+
+        //Laplacian smoothing.
+        Smooth();
+
+        for (int i=0; i<number; i++)
+    	{
+            //TODO: Update X and V here.
+            V[i] = damp * (V[i] + Force[i] / mass * dt);
+            X[i] = X[i] + V[i] * dt;
+
+            //TODO: (Particle) collision with floor.
+            if (X[i].y < -3.0f)
+            {
+                V[i].x = 0;
+                V[i].z = 0;
+                V[i].y += (-3.0f - X[i].y) / dt;
+                X[i].y = -3.0f;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	for(int l=0; l<10; l++)
+    		 _Update();
+
+    	// Dump the vertex array for rendering.
+    	Vector3[] vertices = new Vector3[tet_number*12];
+        int vertex_number=0;
+        for(int tet=0; tet<tet_number; tet++)
+        {
+        	vertices[vertex_number++]=X[Tet[tet*4+0]];
+        	vertices[vertex_number++]=X[Tet[tet*4+2]];
+        	vertices[vertex_number++]=X[Tet[tet*4+1]];
+        	vertices[vertex_number++]=X[Tet[tet*4+0]];
+        	vertices[vertex_number++]=X[Tet[tet*4+3]];
+        	vertices[vertex_number++]=X[Tet[tet*4+2]];
+        	vertices[vertex_number++]=X[Tet[tet*4+0]];
+        	vertices[vertex_number++]=X[Tet[tet*4+1]];
+        	vertices[vertex_number++]=X[Tet[tet*4+3]];
+        	vertices[vertex_number++]=X[Tet[tet*4+1]];
+        	vertices[vertex_number++]=X[Tet[tet*4+2]];
+        	vertices[vertex_number++]=X[Tet[tet*4+3]];
+        }
+        Mesh mesh = GetComponent<MeshFilter> ().mesh;
+		mesh.vertices  = vertices;
+		mesh.RecalculateNormals ();
+    }
+}
